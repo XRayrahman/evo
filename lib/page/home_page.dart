fd1b42f34805a861e857452a974ac4a248a6f8d3
XRayrahman <rayadityarahman14@gmail.comgit>
2022-07-30 22:52:28 +0700

    update home layout

--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -1,5 +1,9 @@
+import 'package:evo/status_controller.dart';
 import 'package:evo/widget/app_bar.dart';
+import 'package:evo/widget/battery.dart';
 import 'package:evo/widget/list_card.dart';
+import 'package:evo/widget/status_card.dart';
+import 'package:evo/widget/status_mini_card.dart';
 import 'package:flutter/material.dart';
 import 'package:line_icons/line_icons.dart';
 import 'package:evo/main_controller.dart';
@@ -11,7 +15,8 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final MainController c = Get.put(MainController());
+    final MainController c = Get.find();
+    final StatusController s = Get.put(StatusController());
     return Scaffold(
       backgroundColor: Colors.grey[300],
       body: Center(
@@ -33,19 +38,33 @@ class HomePage extends StatelessWidget {
               Expanded(
                   child: NeuCard(
                 childs: Container(
-                  padding: EdgeInsets.all(10),
+                  padding: const EdgeInsets.all(10),
                   child: Row(
                     crossAxisAlignment: CrossAxisAlignment.stretch,
                     children: [
                       Expanded(
-                        child: Container(
-                          child: ClipRRect(
-                            borderRadius:
-                                const BorderRadius.all(Radius.circular(15)),
-                            child: Container(
-                                padding: const EdgeInsets.all(20),
-                                // color: Colors.amber,
-                                child: const Text("Battery")),
+                        child: GestureDetector(
+                          onTap: () => s.onPressed(70),
+                          // color: Colors.amber,
+                          // padding: EdgeInsets.all(10),
+                          child: StatusCard(
+                            title: "Battery",
+                            childs: Column(
+                              children: [
+                                Container(
+                                    alignment: Alignment.centerLeft,
+                                    // padding: EdgeInsets.fromLTRB(10, 5, 10, 10),
+                                    child: Obx(
+                                      () => Text(
+                                        "${s.batteryValue.value}%",
+                                        style: const TextStyle(fontSize: 50),
+                                      ),
+                                    )),
+                                const BatteryWidget(
+                                  value: 50,
+                                ),
+                              ],
+                            ),
                           ),
                         ),
                       ),
@@ -53,21 +72,23 @@ class HomePage extends StatelessWidget {
                         child: Column(
                           crossAxisAlignment: CrossAxisAlignment.stretch,
                           // mainAxisAlignment: ,
-                          children: [
+                          children: const [
                             Expanded(
-                              child: Container(
-                                child: Container(
-                                    padding: const EdgeInsets.all(20),
-                                    // color: Colors.amber,
-                                    child: const Text("Temperature")),
+                              child: StatusMiniCard(
+                                title: "°C",
+                                childs: Text(
+                                  "34",
+                                  style: TextStyle(fontSize: 40),
+                                ),
                               ),
                             ),
                             Expanded(
-                              child: Container(
-                                child: Container(
-                                    padding: const EdgeInsets.all(20),
-                                    // color: Colors.amber,
-                                    child: const Text("Voltage")),
+                              child: StatusMiniCard(
+                                title: "V ",
+                                childs: Text(
+                                  "74",
+                                  style: TextStyle(fontSize: 40),
+                                ),
                               ),
                             ),
                           ],
@@ -80,7 +101,7 @@ class HomePage extends StatelessWidget {
               ListCard(
                 title: "Bluetooth",
                 subtitle: "connect with dashboard",
-                subtitleStyle: TextStyle(),
+                subtitleStyle: const TextStyle(),
                 childs: Row(children: [
                   IconButton(
                     onPressed: () {},
@@ -91,7 +112,7 @@ class HomePage extends StatelessWidget {
               ListCard(
                 title: "Navigation",
                 subtitle: "estimate travel time and lineage",
-                subtitleStyle: TextStyle(),
+                subtitleStyle: const TextStyle(),
                 childs: IconButton(
                     onPressed: () {
                       c.goToPage("Navigation");
